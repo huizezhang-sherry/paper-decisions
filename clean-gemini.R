f9b2c9c9df9b2cff1b7b5ae242874f0d1bb82244
huizezhang-sherry <huizezhangsh@gmail.com>
2025-08-24 20:53:57 -0500

    clean up computing

--- clean-gemini.R
+++ clean-gemini.R
@@ -4,7 +4,7 @@ paper_new <- map_dfr(md_files, function(x){
   as_decision_tbl(x = here::here(paste0(root, x)))
 }, .id = "id")
 
-gemini_r1_raw <- tibble(id = as.character(1:length(md_files)),
+gemini_v1_raw <- tibble(id = as.character(1:length(md_files)),
                         paper = str_remove(md_files, ".md")) |>
   left_join(paper_new, by = c("id", "paper")) |>
   select(-id) |>
@@ -12,78 +12,28 @@ gemini_r1_raw <- tibble(id = as.character(1:length(md_files)),
   mutate(id = row_number(), paper = str_remove(paper, "-gemini-1")) |>
   ungroup() |>
   select(paper, id, model:decision, reference)
+save(gemini_v1_raw, file = here::here("data/gemini_v1_raw.rda"))
 
-gemini_r1_raw <- gemini_r1_raw |>
-  mutate(model = ifelse(paper == "anderson2001particulate", "Generalised additive models", model),
-         model = ifelse(paper %in% c("bell2007effect", "lisabeth2008ambient", "burnett2004associations", "franklin2008role",
-                                     "peng2008coarse",
+##################################################################
+##################################################################
+# validate and edit LLM output
+# bell2007effect use GLM
+gemini_edited <- gemini_v1_raw |>
+  mutate(
+         model = ifelse(paper %in% c("lisabeth2008ambient","burnett2004associations",
+                                     "franklin2008role", "peng2008coarse",
                                      "ostro2006fine", "peel2005ambient"), "GLM", model),
          model = ifelse(paper == "bell2008seasonal", "bayesian hierarchical model", model),
-         model = ifelse(paper == "breitner2009short", "generalized semiparametric poisson regression models", model),
-         model = ifelse(paper %in% c("burnett1998association", "burnett2001association",
-                                     "castillejos2016airborne", "cifuentes2011effect",
+         model = ifelse(paper %in% c("bell2007effect", "anderson2001particulate",
+                                     "breitner2009short", "burnett1998association",
+                                     "burnett2001association", "castillejos2016airborne", "cifuentes2011effect",
                                      "neuberger2007extended", "schwartz2002concentration",
                                      "stolzel2008daily", "zanobetti2003temporal","zanobetti2003temporal"),
-                        "generalized additive model", model)
-         )
+                        "generalized additive model", model),
 
-gemini_df |> distinct(paper, model) |> View()
-
-gemini_df_raw <- gemini_r1_raw |>
-  filter(!model %in% c("case-crossover design", "unconstrained distributed lag model",
-                       "regression tree", "lag distributed model", "unconstrained distributed lag models")) |>
-  filter(!str_detect(model, "distributed lag|alternative weather model")) |>
-  # 2) no method or parameter for type spatial/temporal
-  mutate(method = ifelse(type %in% c("spatial", "temporal"), NA, method),
-         parameter = ifelse(type %in% c("spatial", "temporal"), NA, parameter)) |>
-  # 5) misc pre-processing
-  mutate(variable = str_replace_all(variable, " ", "_"),
-         variable = str_replace_all(variable, "dew-point", "dewpoint"),
-         variable = str_replace_all(variable, "dew_point", "dewpoint")) |>
-  # 3) group variables
-  mutate(
-    model = str_to_lower(model),
-    model = ifelse(str_detect(model, "generalized additive|generalised additive|gam|general additive"), "GAM", model),
-    model = ifelse(str_detect(model, "generalized linear model|glm|poisson regression|general linear"), "GLM", model),
-    variable = ifelse(str_detect(variable, "pollutant|pollution|particulate|NCtot|exposure"), "PM", variable),
-    variable = ifelse(str_detect(variable, "PM"), "PM", variable),
-    variable = ifelse(variable %in% c("dewpoint temperature", "dewpoint_temperature", "dewpoint", "RH"),
-                      "humidity", variable),
-    variable = ifelse(str_detect(variable, "humidity"), "humidity", variable),
-    variable = ifelse(str_detect(variable, "time|trends|trend|date"), "time", variable),
-    variable = ifelse(str_detect(variable, "temperature"), "temperature", variable),
-  ) |>
-  # 4) standardize method and parameter
-  mutate(method = ifelse(method %in% c("spline smoother", "spline smooth functions"),
-                         "smoothing spline", method),
-         method = ifelse(str_detect(method, "penalized|penalised"), "smoothing spline", method),
-         method = ifelse(str_detect(method, "smoothing"), "smoothing spline", method),
-         method = ifelse(method %in% c("cubic splines", "cubic regression spline",
-                                       "cubic regression splines", "cubic spline"),
-                         "natural spline", method),
-         method = ifelse(str_detect(method, "cubic"), "natural spline", method),
-         method = ifelse(str_detect(method, "natural"), "natural spline", method),
-         method = ifelse(method %in% c("loess", "LOESS"), "LOESS", method),
-         method = ifelse(str_detect(method, "nonparametric"), "LOESS", method),
-         method = ifelse(str_detect(method, "non-parametric"), "LOESS", method),
-         parameter = ifelse(method == "natural spline", "knots", parameter),
-         parameter = ifelse(method == "LOESS", "smoothing window", parameter),
-         parameter = ifelse(method == "smoothing spline", "smoothing parameter", parameter)) |>
-  mutate(reason = ifelse(reason == "NA", NA_character_, reason),
-         decision = ifelse(decision == "NA", NA_character_, decision))
-
-
-
-#####################################################################################
-#####################################################################################
-#####################################################################################
-
-gemini_df <- gemini_df_raw |>
-  mutate(
     # category: model
     model = ifelse(paper == "jalaludin2009association" & id %in% c(7,8),
-                      "generalized additive model", model), # recode: 42 total df
-
+                   "generalized additive model", model), # recode: 42 total df
 
     # category: time - parameter
     decision = ifelse(paper == "schreuder2006ambient" & variable == "time" & type == "parameter",
@@ -133,12 +83,14 @@ gemini_df <- gemini_df_raw |>
     reason = ifelse(paper == "stolzel2008daily" & variable == "humidity" & type == "temporal",
                     "using minimization of Akaike Information Criteria (AIC)", reason), # fail to capture
 
-
-
     variable = ifelse(paper == "cifuentes2011effect" & variable == "all_variables" & type == "temporal",
                       "PM", variable), # recode: 3 previous day
     variable = ifelse(paper == "cifuentes2011effect" & variable == "all_variables" & type == "temporal",
-                      "PM", variable) # recode: 3 previous day
+                      "PM", variable), # recode: 3 previous day
+
+    # category: method
+    method = ifelse(paper == "anderson2001particulate" & type == "parameter", "LOESS", method), # recode: author
+    method = ifelse(paper == "lee2006association" & type == "parameter", "LOESS", method) # recode: author
   ) |>
   filter(
     # category: time - parameter
@@ -210,6 +162,7 @@ gemini_df <- gemini_df_raw |>
     !(paper == "mar2000associations" & id == 17), # duplicates
     !(paper == "mar2000associations" & id == 18), # duplicates
     !(paper == "mar2000associations" & id == 19), # duplicates
+
     # category: mortality
     !(paper == "castillejos2016airborne" & id == 1), # fail to capture
     !(paper == "castillejos2016airborne" & id == 2), # fail to capture
@@ -230,12 +183,12 @@ gemini_df <- gemini_df_raw |>
     !(paper == "ito2006pm" & id == 17), # irrelevant: secondary model
     !(paper == "ito2006pm" & id == 18), # irrelevant: secondary model
     !(paper == "ito2006pm" & id == 19) # irrelevant: secondary model
-
   ) |>
-  filter(!paper %in% c("burnett1998association", "burnett2001association", "burnett2004associations", "linares2010short",
+  filter(!paper %in% c("burnett1998association", "burnett2001association",
+                       "burnett2004associations", "linares2010short",
                        "mate2010short"))
 
-gemini_df <- gemini_df |>
+gemini_edited <- gemini_edited |>
   bind_rows(tibble(paper = "chen2008influence", id = 13, model = "general linear model", variable = "temperature",
                    method = NA, parameter = NA, type = "temporal", reason = NA, decision = "first 3 days"), # recode
             tibble(paper = "chen2008influence", id = 13, model = "general linear model", variable = "humidity",
@@ -270,9 +223,6 @@ gemini_df <- gemini_df |>
                    method = NA, parameter = NA, type = "temporal", reason = NA, decision = "average of previous day up to 3 previous days"), # recode
 
 
-
-
-
             tibble(paper = "neuberger2007extended", id = 8, model = "overdispersed Poisson GAM", variable = "temperature",
                    method = NA, parameter = NA, type = "parameter", reason = "selected on teh basis of the Akaike information criterion", decision = NA), # fail to capture
             tibble(paper = "neuberger2007extended", id = 8, model = "overdispersed Poisson GAM", variable = "humidity",
@@ -283,13 +233,104 @@ gemini_df <- gemini_df |>
   )
 
 
-gemini_df <- gemini_df |>
+gemini_std <- gemini_edited |>
+  filter(!model %in% c("case-crossover design", "distributed lag", "regression tree", "lag distributed")) |>
+  filter(!str_detect(model, "distributed lag|alternative weather model")) |>
+  # 2) no method or parameter for type spatial/temporal
+  mutate(method = ifelse(type %in% c("spatial", "temporal"), NA, method),
+         parameter = ifelse(type %in% c("spatial", "temporal"), NA, parameter)) |>
+  # 5) misc pre-processing
+  mutate(variable = str_replace_all(variable, " ", "_"),
+         variable = str_replace_all(variable, "dew-point", "dewpoint"),
+         variable = str_replace_all(variable, "dew_point", "dewpoint")) |>
+  # 3) group variables
+  mutate(
+    model = str_to_lower(model),
+    model = ifelse(str_detect(model, "generalized additive|generalised additive|gam|general additive"), "GAM", model),
+    model = ifelse(str_detect(model, "generalized linear model|glm|poisson regression|general linear"), "GLM", model),
+    variable = ifelse(str_detect(variable, "pollutant|pollution|particulate|NCtot|exposure"), "PM", variable),
+    variable = ifelse(str_detect(variable, "PM"), "PM", variable),
+    variable = ifelse(variable %in% c("dewpoint temperature", "dewpoint_temperature", "dewpoint", "RH"),
+                      "humidity", variable),
+    variable = ifelse(str_detect(variable, "humidity"), "humidity", variable),
+    variable = ifelse(str_detect(variable, "time|trends|trend|date"), "time", variable),
+    variable = ifelse(str_detect(variable, "temperature"), "temperature", variable),
+  ) |>
+  # 4) standardize method and parameter
+  mutate(method = ifelse(method %in% c("spline smoother", "spline smooth functions"),
+                         "smoothing spline", method),
+         method = ifelse(str_detect(method, "penalized|penalised"), "smoothing spline", method),
+         method = ifelse(str_detect(method, "smoothing"), "smoothing spline", method),
+         method = ifelse(method %in% c("cubic splines", "cubic regression spline",
+                                       "cubic regression splines", "cubic spline", "cubic"),
+                         "natural spline", method),
+         method = ifelse(str_detect(method, "cubic"), "natural spline", method),
+         method = ifelse(str_detect(method, "natural"), "natural spline", method),
+         method = ifelse(method %in% c("loess", "LOESS"), "LOESS", method),
+         method = ifelse(str_detect(method, "nonparametric"), "LOESS", method),
+         method = ifelse(str_detect(method, "non-parametric"), "LOESS", method),
+         parameter = ifelse(method == "natural spline", "knots", parameter),
+         parameter = ifelse(method == "LOESS", "smoothing window", parameter),
+         parameter = ifelse(method == "smoothing spline", "smoothing parameter", parameter)) |>
+  mutate(reason = ifelse(reason == "NA", NA_character_, reason),
+         decision = ifelse(decision == "NA", NA_character_, decision))
+
+
+#####################################################################################
+#####################################################################################
+#####################################################################################
+# secondary standardization on the temporal decisions
+library(ellmer)
+type_named_lag <- type_object(
+  operation = type_string("mean, average or NA"),
+  lag_start = type_string("first lag, usually 0"),
+  lag_end = type_string("last lag")
+  )
+
+# #Using model = "claude-3-7-sonnet-latest".
+# chat <- chat_claude()
+#
+# temporal_claude_raw <- gemini_std |>
+#   filter(type == "temporal") |>
+#   rowwise() |>
+#   mutate(temp_std = list(parallel_chat_structured(chat, list(decision), type = type_named_lag)))
+
+temporal_claude <- temporal_claude_raw |>
+  unnest_wider(temp_std) |>
+  mutate(operation = ifelse(operation %in% c("average", "mean"), "multi-day", "single-day"),
+         lag_start = as.numeric(lag_start),
+         lag_end = as.numeric(lag_end),
+         lag_end = ifelse(is.na(lag_end), 0, lag_end)) |>
+  rowwise() |>
+  mutate(temporal = ifelse(operation == "multi-day",
+                           paste0("lag ", lag_start, "-", lag_end),
+                           paste0("lag ", seq(lag_start, lag_end, 1), collapse = ", ")),
+         decision_clean = paste0(operation, " ", temporal)) |>
+  ungroup()
+#save(temporal_claude, file = here::here("data/temporal_claude.rda"))
+
+
+gemini_std2 <- temporal_claude |>
+  select(-decision, -c(operation:temporal)) |>
+  rename(decision = decision_clean) |>
+  bind_rows(gemini_std |> filter(type != "temporal")) |>
+  select(paper, id, model, variable, method, parameter, type, reason, decision, reference) |>
+  arrange(paper, id)
+
+
+#write_csv(gemini_df, file = here::here("data/gemini_df.csv"))
+
+
+#####################################################################################
+#####################################################################################
+#####################################################################################
+gemini_df <- gemini_std2 |>
   group_by(paper, model) %>%
   tally(name = "count") %>%
   arrange(paper, desc(count)) %>%
   slice_head(n = 1) %>%
   select(paper, most_common_model = model) %>%
-  right_join(gemini_df, by = "paper") %>%
+  right_join(gemini_std2, by = "paper") %>%
   mutate(model = most_common_model) %>%
   select(-most_common_model) |>
   group_by(paper, variable, type) %>%
@@ -307,54 +348,4 @@ gemini_df <- gemini_df |>
   ) |>
   mutate(reason = str_replace_all(reason, "[^\\x00-\\x7F]", "")) # remove non-ASCII characters
 
-#####################################################################################
-#####################################################################################
-#####################################################################################
-
-temporal_df <- gemini_df |>
-  filter(type == "temporal") |>
-  separate_longer_delim(decision, delim = c("; ")) |>
-  filter(!is.na(decision), !decision %in% c("smooth function")) |>
-  mutate(id = row_number()) |>
-  select(paper, variable, decision, reason)
-
-# not yet on katsouyanni
-# library(ellmer)
-# text <- temporal_df$decision |> as.list()
-# type_named_lag <- type_object(
-#     operation = type_string("mean, average or NA"),
-#     lag_start = type_string("first lag, usually 0"),
-#     lag_end = type_string("last lag")
-#   )
-#
-# #Using model = "claude-3-7-sonnet-latest".
-# chat <- chat_claude()
-# temporal_decision_claude <- parallel_chat_structured(chat, text, type = type_named_lag)
-# save!!!
-
-temporal_decision_df <- temporal_decision_claude |> as_tibble() |>
-  mutate(operation = ifelse(operation %in% c("average", "mean"), "multi-day", "single-day"),
-         lag_start = as.numeric(lag_start),
-         lag_end = as.numeric(lag_end),
-         lag_end = ifelse(is.na(lag_end), 0, lag_end))
-
-temporal_decision_clean <- temporal_decision_df |>
-  rowwise() |>
-  mutate(temporal = ifelse(operation == "multi-day",
-                           paste0("lag ", lag_start, "-", lag_end),
-                           paste0("lag ", seq(lag_start, lag_end, 1), collapse = ", ")),
-         decision_clean = paste0(operation, " ", temporal))
-
-temporal_df_aug <- temporal_df |>
-  bind_cols(temporal_decision_clean |> select(decision_clean))
-
-gemini_df <- gemini_df |>
-  filter(type == "temporal") |>
-  separate_longer_delim(decision, delim = c("; ")) |>
-  left_join(temporal_df_aug |> select(paper, variable, decision, decision_clean),
-            by = c("paper", "variable", "decision")) |>
-  mutate(decision = decision_clean) |>
-  select(-decision_clean) |>
-  bind_rows(gemini_df |> filter(type != "temporal")) |>
-  arrange(paper, variable)
 #write_csv(gemini_df, file = here::here("data/gemini_df.csv"))
